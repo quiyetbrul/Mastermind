bc92096e8a68b27a6063e002f9001184463c4963
Quiyet Brul <qbrul0514@gmail.com>
2024-10-11 21:36:50 -0600

    update window size + add input validation

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -29,3 +29,25 @@ int InputInteger(const std::string prompt, const int &startRange,
   } while (true);
   return input;
 }
+
+char InputChar(const std::string &prompt, const char &yes, const char &no) {
+  char input;
+  do {
+    std::cout << prompt;
+    try {
+      std::cin >> input;
+      input = tolower(input);
+      if (std::cin.fail() || (input != yes && input != no)) {
+        std::cin.clear(); // clear the error flag
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(),
+                        '\n'); // discard invalid input
+        std::cout << "Invalid input. Please enter a valid input between " << yes
+                  << " and " << no << std::endl;
+      } else
+        break;
+    } catch (std::invalid_argument &e) {
+      std::cout << e.what() << std::endl;
+    }
+  } while (true);
+  return input;
+}
--- src/util/util.h
+++ src/util/util.h
@@ -3,15 +3,13 @@
 
 #include <string>
 
-const int kTerminalWidth = 121;
+const int kTerminalWidth = 125;
 const int kTerminalHeight = 24;
 
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_YELLOW "\x1b[33m"
 #define ANSI_COLOR_BLUE "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN "\x1b[36m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 
 void ClearScreen();
@@ -27,5 +25,7 @@ int GenRandom(const int &min, const int &max);
 
 int InputInteger(const std::string prompt, const int &startRange,
                  const int &endRange);
+
+char InputChar(const std::string &prompt, const char &yes, const char &no);
 
 #endif // UTIL_H_
