2bed85260f83c847107eb6f7f5caa3f4de779b88
Quiyet Brul <qbrul0514@gmail.com>
2024-10-14 00:22:12 -0600

    ansi rename + input validation

--- src/util/util.h
+++ src/util/util.h
@@ -3,6 +3,8 @@
 
 #include <string>
 
+// TODO: move to definitions file
+
 const int kTerminalWidth = 125;
 const int kTerminalHeight = 24;
 
@@ -10,7 +12,12 @@ const int kTerminalHeight = 24;
 #define ANSI_COLOR_GREEN "\x1b[32m"
 #define ANSI_COLOR_YELLOW "\x1b[33m"
 #define ANSI_COLOR_BLUE "\x1b[34m"
-#define ANSI_COLOR_RESET "\x1b[0m"
+#define ANSI_RESET "\x1b[0m"
+#define ANSI_BLINK = "\x1b[5m"
+
+const int kMinSecretCode = 0;
+const int kMaxSecretCodeDigit = 7;
+const int kSecretCodeLength = 4;
 
 // TODO: inline these small functions
 
@@ -29,5 +36,7 @@ int InputInteger(const std::string prompt, const int &startRange,
                  const int &endRange);
 
 char InputChar(const std::string &prompt, const char &yes, const char &no);
+
+std::string InputGuess(const std::string &prompt);
 
 #endif // UTIL_H_
