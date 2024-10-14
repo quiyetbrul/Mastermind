65ca6dce603943b39282bf13c81937f2aa93cda2
Quiyet Brul <qbrul0514@gmail.com>
2024-10-13 23:17:19 -0600

    switch dir/ + print file

---
+++ src/ui/menu.cpp
@@ -0,0 +1,33 @@
+#include "menu.h"
+
+#include "../ui/menu.h"
+#include "../ui/print.h"
+
+#include <iostream>
+
+int Menu() {
+  Title();
+  std::cout << "1. Play\n";
+  std::cout << "2. Instructions\n";
+  std::cout << "3. Exit\n";
+  int choice = InputInteger("Enter your choice: ", 1, 3);
+  return choice;
+}
+
+void Instructions() {
+  Title();
+  std::cout << "Instructions:\n";
+  std::cout << "1. The computer will generate a secret code consisting of 4 "
+               "numbers.\n";
+  std::cout << "2. The numbers in the secret code will be between 0 and 7.\n";
+  std::cout << "3. You have to guess the secret code.\n";
+  std::cout << "4. After each guess, the computer will give you feedback.\n";
+  std::cout << "5. The feedback will characters 'X' and 'O':\n";
+  std::cout << "   - 'X' represents digits in correct positions\n";
+  std::cout << "   - 'O' represents digits in incorrect positions\n";
+  std::cout << "6. You have 10 chances to guess the secret code.\n";
+  std::cout << "7. If you guess the secret code within 10 chances, you win.\n";
+  std::cout << "8. If you are unable to guess the secret code within 10 "
+               "chances, you lose.\n";
+  std::cout << "9. Good luck!\n\n";
+}
--- src/ui/print.h
+++ src/ui/print.h
@@ -1,42 +1,10 @@
 // clang-format off
 
-#include "menu.h"
-
 #include "../util/util.h"
 
 #include <iostream>
 
-int Menu() {
-  Title();
-  std::cout << "1. Play\n";
-  std::cout << "2. Instructions\n";
-  std::cout << "3. Exit\n";
-  int choice = InputInteger("Enter your choice: ", 1, 3);
-  return choice;
-}
-
-void Instructions() {
-  Title();
-  std::cout << "Instructions:\n";
-  std::cout << "1. The computer will generate a secret code consisting of 4 "
-               "numbers.\n";
-  std::cout << "2. The numbers in the secret code will be between 0 and 7.\n";
-  std::cout << "3. You have to guess the secret code.\n";
-  std::cout << "4. After each guess, the computer will give you feedback.\n";
-  std::cout << "5. The feedback will consist of 2 numbers:\n";
-  std::cout << "   - The first number represents the number of correct digits "
-               "in the correct position.\n";
-  std::cout << "   - The second number represents the number of correct digits "
-               "in the wrong position.\n";
-  std::cout << "6. You have 10 chances to guess the secret code.\n";
-  std::cout << "7. If you guess the secret code within 10 chances, you win.\n";
-  std::cout << "8. If you are unable to guess the secret code within 10 "
-               "chances, you lose.\n";
-  std::cout << "9. Good luck!\n\n";
-}
-
-// TODO: should probably in a print file
-void Title() {
+inline void Title() {
   ClearScreen();
   // TODO: get terminal width dynamically
   std::cout << "                                      _ _ _ ____ _    ____ ____ _  _ ____     ___ ____\n";
@@ -55,7 +23,7 @@ void Title() {
   std::cout << "                                |__]   |      |_\\| |__| |   |   |___  |     |__] |  \\ |__| |___\n\n";
 }
 
-void Congratulations() {
+inline void Congratulations() {
   ClearScreen();
   std::cout << ANSI_COLOR_GREEN;
   std::cout << " ██████╗ ██████╗ ███╗   ██╗ ██████╗ ██████╗  █████╗ ████████╗██╗   ██╗██╗      █████╗ ████████╗██╗ ██████╗ ███╗   ██╗███████╗\n";
@@ -67,7 +35,7 @@ void Congratulations() {
   std::cout << ANSI_COLOR_RESET;
 }
 
-void TryAgain() {
+inline void TryAgain() {
   ClearScreen();
   std::cout << ANSI_COLOR_RED;
   std::cout << "████████╗██████╗ ██╗   ██╗      █████╗  ██████╗  █████╗ ██╗███╗   ██╗\n";
@@ -79,7 +47,7 @@ void TryAgain() {
   std::cout << ANSI_COLOR_RESET;
 }
 
-void Goodbye() {
+inline void Goodbye() {
   ClearScreen();
   std::cout << ANSI_COLOR_BLUE;
   std::cout << " ██████╗  ██████╗  ██████╗ ██████╗ ██████╗ ██╗   ██╗███████╗\n";
