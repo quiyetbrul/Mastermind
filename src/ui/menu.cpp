3fa01bfb26fd6c1361e52d8c080aa30bfe8a678f
Quiyet Brul <qbrul0514@gmail.com>
2024-10-16 17:45:22 -0600

    Update menu.cpp

--- src/ui/menu.cpp
+++ src/ui/menu.cpp
@@ -17,9 +17,8 @@ int MainMenu() {
 int PlayerMenu() {
   Title();
   std::cout << "1. Single Player\n";
-  std::cout << "2. Multi Player\n";
-  std::cout << "3. Against Computer\n";
-  int choice = InputInteger("Enter your choice: ", 1, 3);
+  std::cout << "2. Computer as Codebreaker\n";
+  int choice = InputInteger("Enter your choice: ", 1, 2);
   return choice;
 }
 
@@ -31,9 +30,9 @@ void Instructions() {
   std::cout << "2. The numbers in the secret code will be between 0 and 7.\n";
   std::cout << "3. You have to guess the secret code.\n";
   std::cout << "4. After each guess, the computer will give you feedback.\n";
-  std::cout << "5. The feedback will characters 'X' and 'O':\n";
-  std::cout << "   - 'X' represents digits in correct positions\n";
-  std::cout << "   - 'O' represents digits in incorrect positions\n";
+  std::cout << "5. The feedback will characters 'B' and 'W':\n";
+  std::cout << "   - 'B' represents digits in correct positions\n";
+  std::cout << "   - 'W' represents digits in incorrect positions\n";
   std::cout << "6. You have 10 chances to guess the secret code.\n";
   std::cout << "7. If you guess the secret code within 10 chances, you win.\n";
   std::cout << "8. If you are unable to guess the secret code within 10 "
