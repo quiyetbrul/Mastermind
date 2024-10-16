7979b77f303a0951114c6db2fba4410d2c3fb8ce
Quiyet Brul <qbrul0514@gmail.com>
2024-10-16 17:45:11 -0600

    handle server busy + make singleton

--- src/util/random_integers.cpp
+++ src/util/random_integers.cpp
@@ -1,25 +1,57 @@
 #include "util.h"
 
-#include <cstdlib>
+#include <algorithm>
 #include <curl/curl.h>
 #include <iostream>
 #include <random>
+#include <string>
 
-// FOLLOWED EXAMPLE FROM https://terminalroot.com/using-curl-with-cpp/
-// Helper function to write the response data
+// Singleton CURL manager class
+class CurlManager {
+public:
+  static CurlManager &Instance() {
+    static CurlManager instance;
+    return instance;
+  }
+
+  CURL *GetCurlHandle() {
+    if (!curl) {
+      curl = curl_easy_init();
+      if (!curl) {
+        std::cerr << "curl_easy_init() failed" << std::endl;
+      }
+    }
+    return curl;
+  }
+
+  ~CurlManager() {
+    if (curl) {
+      curl_easy_cleanup(curl);
+    }
+  }
+
+private:
+  CURL *curl = nullptr;
+
+  CurlManager() = default;
+  CurlManager(const CurlManager &) = delete;
+  CurlManager &operator=(const CurlManager &) = delete;
+};
+
+// Callback function to handle the response
 static size_t WriteCallback(void *contents, size_t size, size_t nmemb,
                             void *userp) {
   ((std::string *)userp)->append((char *)contents, size * nmemb);
   return size * nmemb;
 }
 
-// Function to generate a random number using Random.org API
+// Function to generate random numbers using Random.org API
 std::string GenRandom(const int &generate, const int &min, const int &max) {
   CURL *curl;
   CURLcode res;
   std::string read_buffer;
 
-  curl = curl_easy_init();
+  curl = CurlManager::Instance().GetCurlHandle();
   if (curl) {
     std::string url =
         "https://www.random.org/integers/?num=" + std::to_string(generate) +
@@ -28,14 +60,20 @@ std::string GenRandom(const int &generate, const int &min, const int &max) {
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
+
     res = curl_easy_perform(curl);
-    curl_easy_cleanup(curl);
 
     if (res == CURLE_OK) {
-      read_buffer.erase(
-          std::remove(read_buffer.begin(), read_buffer.end(), '\n'),
-          read_buffer.end());
-      return read_buffer;
+      long http_code = 0;
+      curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+      if (http_code == 503) {
+        std::cerr << "Server is busy (503 Service Unavailable)" << std::endl;
+      } else {
+        read_buffer.erase(
+            std::remove(read_buffer.begin(), read_buffer.end(), '\n'),
+            read_buffer.end());
+        return read_buffer;
+      }
     } else {
       std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res)
                 << std::endl;
@@ -52,12 +90,10 @@ std::string GenRandom(const int &generate, const int &min, const int &max) {
   return random_number;
 }
 
+// Local random number generator
 int RandomNumber(const int &min, const int &max) {
-  // Seed for the random number engine
   std::random_device rd;
-  // Mersenne Twister engine seeded with rd()
   std::mt19937 gen(rd());
-  // Distribution in range [min, max]
   std::uniform_int_distribution<> dis(min, max);
   return dis(gen);
 }
