2f978e7dab61e51b785b6deb5225628a8da24af1
Quiyet Brul <qbrul0514@gmail.com>
2024-10-17 23:18:10 -0600

    replace player info with player obj

--- src/games/games.h
+++ src/games/games.h
@@ -2,7 +2,6 @@
 #define GAMES_H_
 
 #include <string>
-#include <vector>
 
 #include "../player/player.h"
 
@@ -11,46 +10,22 @@ public:
   Games(){};
 
   Games(std::string game_name_, std::string game_password_,
-        const Player &player)
-      : game_name_(game_name_), game_password_(game_password_) {}
-
-  Games(std::string game_name_, std::string game_password_, int player_one_life,
-        int player_one_score_, std::string player_one_secret_code_,
-        std::vector<std::pair<std::string, std::string>> player_one_guesses_)
+        const Player &player_)
       : game_name_(game_name_), game_password_(game_password_),
-        player_one_life_(player_one_life), player_one_score_(player_one_score_),
-        player_one_secret_code_(player_one_secret_code_),
-        player_one_guesses_(player_one_guesses_) {}
+        player_(player_) {}
 
   // getters
-  std::string GetGameSaveTime() const;
   std::string GetGameName() const;
   std::string GetGamePassword() const;
-  int GetPlayerOneLife() const;
-  int GetPlayerOneScore() const;
-  std::string GetPlayerOneSecretCode() const;
-  std::vector<std::pair<std::string, std::string>> GetPlayerOneGuesses() const;
 
   // setters
-  void SetGameSaveTime(const std::string &game_save_time);
   void SetGameName(const std::string &game_name);
   void SetGamePassword(const std::string &game_password);
-  void SetPlayerOneLife(const int &player_one_life);
-  void SetPlayerOneScore(const int &player_one_score);
-  void SetPlayerOneSecretCode(const std::string &player_one_secret_code);
-  void
-  SetPlayerOneGuesses(const std::vector<std::pair<std::string, std::string>>
-                          &player_one_guesses);
 
 private:
   std::string game_name_;
   std::string game_password_;
-
-  int player_one_life_ = 0;
-  int player_one_score_ = 0;
-  // TODO: turn into array instead of vector
-  std::vector<std::pair<std::string, std::string>> player_one_guesses_;
-  std::string player_one_secret_code_;
+  Player player_;
 };
 
 #endif // GAMES_H_
