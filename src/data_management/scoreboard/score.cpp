c9c3ffdf62e9eacab8ac4b5bc5afb8381b26aa8d
Quiyet Brul <qbrul0514@gmail.com>
2024-11-16 23:03:04 -0700

    Update score.cpp

--- src/data_management/scoreboard/score.cpp
+++ src/data_management/scoreboard/score.cpp
@@ -35,46 +35,53 @@ void Score::PrintScores() const {
   int x;
   getmaxyx(window_, y, x);
   y = 1;
+  x /= 2;
   wclear(window_);
   std::string title = "Top Scores";
-  mvwprintw(window_, y++, (x / 2) - (title.length() / 2), title.c_str());
+  mvwprintw(window_, y++, x - (title.length() / 2), title.c_str());
   if (GetCount() < 1) {
     std::string no_score = "No scores yet!";
-    mvwprintw(window_, y++, (x / 2) - (no_score.length() / 2),
-              no_score.c_str());
+    mvwprintw(window_, y++, x - (no_score.length() / 2), no_score.c_str());
   } else {
     // Determine the maximum length of user names
     SQLite::Statement max_length_query(
-        db_, "SELECT MAX(LENGTH(USER_NAME)) FROM " + GetTableName() + ";");
+        db_, "SELECT USER_NAME FROM " + GetTableName() +
+                 " ORDER BY LENGTH(USER_NAME) DESC LIMIT 1;");
     max_length_query.executeStep();
     int max_name_length = max_length_query.getColumn("USER_NAME").getInt();
 
-    std::string header[] = {"Name", "Score", "Time", "Difficulty"};
-
-    x = 2; // Starting x position for printing
     int col_width = max_name_length + x;
 
+    int temp = x;
+    std::vector<std::string> header = {"Name", "Score", "Time", "Difficulty"};
+    // TODO: can probably refactor this to a function
+    int total_width = header.size() * max_name_length;
     for (const auto &head : header) {
-      mvwprintw(window_, y, x, head.c_str());
-      x += col_width;
+      temp += col_width;
+      mvwprintw(window_, y, (temp / 4) + total_width, head.c_str());
     }
     ++y;
 
     SQLite::Statement query(db_, "SELECT * FROM " + GetTableName() +
                                      " ORDER BY SCORE DESC, ELAPSED_TIME ASC;");
     while (query.executeStep()) {
-      x = 2;
-      mvwprintw(window_, y, x, "%s", query.getColumn("USER_NAME").getText());
-      x += col_width;
-      mvwprintw(window_, y, x, "%d", query.getColumn("SCORE").getInt());
-      x += col_width;
-      mvwprintw(window_, y, x, "%.2fs",
+      temp = x;
+      temp += col_width;
+      mvwprintw(window_, y, (temp / 4) + total_width, "%s",
+                query.getColumn("USER_NAME").getText());
+      temp += col_width;
+      mvwprintw(window_, y, (temp / 4) + total_width, "%d",
+                query.getColumn("SCORE").getInt());
+      temp += col_width;
+      mvwprintw(window_, y, (temp / 4) + total_width, "%.2fs",
                 query.getColumn("ELAPSED_TIME").getDouble());
-      x += col_width;
-      mvwprintw(window_, y, x, "%d", query.getColumn("DIFFICULTY").getInt());
+      temp += col_width;
+      mvwprintw(window_, y, (temp / 4) + total_width, "%d",
+                query.getColumn("DIFFICULTY").getInt());
       ++y;
     }
   }
+
   EnterToContinue(window_, y);
 }
 
