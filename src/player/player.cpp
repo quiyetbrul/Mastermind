60200fc146b605a3367eb2adf379dd1bc66fa111
Quiyet Brul <qbrul0514@gmail.com>
2024-11-20 15:55:32 -0700

    inline getter/setter

--- src/player/player.cpp
+++ src/player/player.cpp
@@ -9,24 +9,9 @@
 #include <cmath>
 #include <string>
 
-#include "logger/logger.h"
 #include "player/util/util.h"
 
 namespace player {
-const int kLifeStart = 10;
-
-const int kEasyMinDigit = 0;
-const int kEasyMaxDigit = 7;
-const int kEasyCodeLength = 4;
-
-const int kMediumMinDigit = 0;
-const int kMediumMaxDigit = 8;
-const int kMediumCodeLength = 4;
-
-const int kHardMinDigit = 0;
-const int kHardMaxDigit = 9;
-const int kHardCodeLength = 4;
-
 Player::Player()
     : life_(kLifeStart), score_(kLifeStart), guess_history_(),
       elapsed_time_(0.0) {
@@ -57,14 +42,6 @@ void Player::EndTime() {
   end_time_ = std::chrono::high_resolution_clock::now();
 }
 
-double Player::GetStartTime() const {
-  return start_time_.time_since_epoch().count();
-}
-
-double Player::GetEndTime() const {
-  return end_time_.time_since_epoch().count();
-}
-
 void Player::SaveElapsedTime() {
   std::chrono::duration<double> elapsed = end_time_ - start_time_;
   elapsed_time_ = std::round(elapsed.count() * 1000.0) / 1000.0;
@@ -84,114 +61,5 @@ void Player::AddToGuessHistory(const std::vector<int> &guess) {
 
 void Player::AddToHintHistory(const std::string &hint) {
   hint_history_.push_back(hint);
-}
-
-std::string Player::GetPlayerName() const { return player_name_; }
-
-std::string Player::GetGameName() const { return game_name_; }
-
-int Player::GetLife() const { return life_; }
-
-int Player::GetScore() const { return score_; }
-
-int Player::GetSecretCodeMinDigit() const { return secret_code_min_digit_; }
-
-int Player::GetSecretCodeMaxDigit() const { return secret_code_max_digit_; }
-
-int Player::GetSecretCodeLength() const { return secret_code_length_; }
-
-int Player::GetDifficulty() const { return difficulty_; }
-
-std::vector<int> Player::GetSecretCode() const { return secret_code_; }
-
-std::vector<std::pair<std::vector<int>, std::string>>
-Player::GetGuesses() const {
-  return guess_history_;
-}
-
-double Player::GetElapsedTime() const { return elapsed_time_; }
-
-int Player::GetHintCount() const { return hint_count_; }
-
-std::vector<std::string> Player::GetHintHistory() const {
-  return hint_history_;
-}
-
-std::string Player::GetLastFeedBack() const { return last_feedback_; }
-
-void Player::SetPlayerName(const std::string &name) { player_name_ = name; }
-
-void Player::SetGameName(const std::string &name) { game_name_ = name; }
-
-void Player::SetLife(const int &life) { life_ = life; }
-
-void Player::SetScore(const int &score) { score_ = score; }
-
-void Player::SetStartTime(const double &start_time) {
-  start_time_ = std::chrono::time_point<std::chrono::high_resolution_clock>(
-      std::chrono::nanoseconds(static_cast<long long>(start_time)));
-}
-
-void Player::SetEndTime(const double &end_time) {
-  end_time_ = std::chrono::time_point<std::chrono::high_resolution_clock>(
-      std::chrono::nanoseconds(static_cast<long long>(end_time)));
-}
-
-void Player::SetElapsedTime(const double &elapsed_time) {
-  elapsed_time_ = elapsed_time;
-}
-
-void Player::SetSecretCodeMinDigit(const int &min_digit) {
-  secret_code_min_digit_ = min_digit;
-}
-
-void Player::SetSecretCodeMaxDigit(const int &max_digit) {
-  secret_code_max_digit_ = max_digit;
-}
-
-void Player::SetSecretCodeLength(const int &length) {
-  secret_code_length_ = length;
-}
-
-void Player::SetSecretCode(const std::vector<int> &secret_code) {
-  secret_code_ = secret_code;
-}
-
-void Player::SetGuesses(
-    const std::vector<std::pair<std::vector<int>, std::string>>
-        &guess_history) {
-  guess_history_ = guess_history;
-}
-
-void Player::SetHintCount(const int &hint_count) { hint_count_ = hint_count; }
-
-void Player::SetHintHistory(const std::vector<std::string> &hint_history) {
-  hint_history_ = hint_history;
-}
-
-void Player::SetLastFeedBack(const std::string &feedback) {
-  last_feedback_ = feedback;
-}
-
-void Player::SetDifficulty(const int &difficulty) {
-  switch (difficulty) {
-  case kEasyDifficulty:
-    SetSecretCodeMinDigit(kEasyMinDigit);
-    SetSecretCodeMaxDigit(kEasyMaxDigit);
-    SetSecretCodeLength(kEasyCodeLength);
-    break;
-  case kMediumDifficulty:
-    SetSecretCodeMinDigit(kMediumMinDigit);
-    SetSecretCodeMaxDigit(kMediumMaxDigit);
-    SetSecretCodeLength(kMediumCodeLength);
-    break;
-  case kHardDifficulty:
-    SetSecretCodeMinDigit(kHardMinDigit);
-    SetSecretCodeMaxDigit(kHardMaxDigit);
-    SetSecretCodeLength(kHardCodeLength);
-    break;
-  }
-  logger_.Log("Difficulty set to " + std::to_string(difficulty));
-  difficulty_ = difficulty;
 }
 } // namespace player
--- src/player/player.h
+++ src/player/player.h
@@ -13,6 +13,8 @@
 
 #include "logger/logger.h"
 
+#include <ncurses.h>
+
 namespace player {
 /**
  * @class Player
@@ -61,53 +63,101 @@ public:
    */
   virtual void GameLoop() = 0;
 
+  /**
+   * @brief Checks if the game is finished.
+   *
+   * @return true If the game is finished.
+   * @return false If the game is not finished.
+   */
   bool IsGameFinished() const;
 
   // GETTERS
 
-  std::string GetPlayerName() const;
-  std::string GetGameName() const;
-  int GetLife() const;
-  int GetScore() const;
-  int GetSecretCodeMinDigit() const;
-  int GetSecretCodeMaxDigit() const;
-  int GetSecretCodeLength() const;
-  int GetDifficulty() const;
-  double GetStartTime() const;
-  double GetEndTime() const;
-  double GetElapsedTime() const;
-  std::vector<int> GetSecretCode() const;
-  std::vector<std::pair<std::vector<int>, std::string>> GetGuesses() const;
-  int GetHintCount() const;
-  std::vector<std::string> GetHintHistory() const;
-  std::string GetLastFeedBack() const;
+  std::string GetPlayerName() const { return player_name_; }
+  std::string GetGameName() const { return game_name_; }
+  int GetGameId() const { return game_id_; }
+  int GetLife() const { return life_; }
+  int GetScore() const { return score_; }
+  int GetSecretCodeMinDigit() const { return secret_code_min_digit_; }
+  int GetSecretCodeMaxDigit() const { return secret_code_max_digit_; }
+  int GetSecretCodeLength() const { return secret_code_length_; }
+  int GetDifficulty() const { return difficulty_; }
+  double GetStartTime() const { return start_time_.time_since_epoch().count(); }
+  double GetEndTime() const { return end_time_.time_since_epoch().count(); }
+  double GetElapsedTime() const { return elapsed_time_; }
+  std::vector<int> GetSecretCode() const { return secret_code_; }
+  std::vector<std::pair<std::vector<int>, std::string>> GetGuesses() const {
+    return guess_history_;
+  }
+  int GetHintCount() const { return hint_count_; }
+  std::vector<std::string> GetHintHistory() const { return hint_history_; }
+  std::string GetLastFeedBack() const { return last_feedback_; }
 
   // SETTERS
 
-  void SetPlayerName(const std::string &name);
-  void SetGameName(const std::string &name);
-  void SetLife(const int &life);
-  void SetScore(const int &score);
-  void SetStartTime(const double &start_time);
-  void SetEndTime(const double &end_time);
-  void SetElapsedTime(const double &elapsed_time);
-  void SetSecretCodeMinDigit(const int &min_digit);
-  void SetSecretCodeMaxDigit(const int &max_digit);
-  void SetSecretCodeLength(const int &length);
-  void SetSecretCode(const std::vector<int> &secret_code);
+  void SetWindow(WINDOW *window) { window_ = window; }
+  void SetPlayerName(const std::string &name) { player_name_ = name; }
+  void SetGameName(const std::string &name) { game_name_ = name; }
+  void SetGameId(const int &id) { game_id_ = id; }
+  void SetLife(const int &life) { life_ = life; }
+  void SetScore(const int &score) { score_ = score; }
+  void SetStartTime(const double &start_time) {
+    start_time_ = std::chrono::time_point<std::chrono::high_resolution_clock>(
+        std::chrono::nanoseconds(static_cast<long long>(start_time)));
+  }
+  void SetEndTime(const double &end_time) {
+    end_time_ = std::chrono::time_point<std::chrono::high_resolution_clock>(
+        std::chrono::nanoseconds(static_cast<long long>(end_time)));
+  }
+  void SetElapsedTime(const double &elapsed_time) {
+    elapsed_time_ = elapsed_time;
+  }
+  void SetSecretCodeMinDigit(const int &min_digit) {
+    secret_code_min_digit_ = min_digit;
+  }
+  void SetSecretCodeMaxDigit(const int &max_digit) {
+    secret_code_max_digit_ = max_digit;
+  }
+  void SetSecretCodeLength(const int &length) { secret_code_length_ = length; }
+  void SetSecretCode(const std::vector<int> &secret_code) {
+    secret_code_ = secret_code;
+  }
   void SetGuesses(
-      const std::vector<std::pair<std::vector<int>, std::string>> &guesses);
-  void SetDifficulty(const int &difficulty);
-  void SetHintCount(const int &hint_count);
-  void SetHintHistory(const std::vector<std::string> &hint_history);
-  void SetLastFeedBack(const std::string &feedback);
+      const std::vector<std::pair<std::vector<int>, std::string>> &guesses) {
+    guess_history_ = guesses;
+  }
+  void SetDifficulty(const int &difficulty) {
+    switch (difficulty) {
+    case 1: // Easy
+      SetSecretCodeMinDigit(kEasyMinDigit);
+      SetSecretCodeMaxDigit(kEasyMaxDigit);
+      SetSecretCodeLength(kEasyCodeLength);
+      break;
+    case 2: // Medium
+      SetSecretCodeMinDigit(kMediumMinDigit);
+      SetSecretCodeMaxDigit(kMediumMaxDigit);
+      SetSecretCodeLength(kMediumCodeLength);
+      break;
+    case 3: // Hard
+      SetSecretCodeMinDigit(kHardMinDigit);
+      SetSecretCodeMaxDigit(kHardMaxDigit);
+      SetSecretCodeLength(kHardCodeLength);
+      break;
+    }
+    logger_.Log("Difficulty set to " + std::to_string(difficulty));
+    difficulty_ = difficulty;
+  }
+  void SetHintCount(const int &hint_count) { hint_count_ = hint_count; }
+  void SetHintHistory(const std::vector<std::string> &hint_history) {
+    hint_history_ = hint_history;
+  }
+  void SetLastFeedBack(const std::string &feedback) {
+    last_feedback_ = feedback;
+  }
 
 protected:
-  static constexpr int kEasyDifficulty = 1;
-  static constexpr int kMediumDifficulty = 2;
-  static constexpr int kHardDifficulty = 3;
-
   Logger &logger_ = Logger::GetInstance();
+  WINDOW *window_;
 
   /**
    * @brief Decrements the player's life.
@@ -142,23 +192,38 @@ protected:
 
   /**
    * @brief Adds a guess to the history.
-
-  * This checks whether the guess already exists in the history. If it does, it
-  * does not add it. Otherwise, it adds the guess to the history and updates
-  * last_feedback_.
+   *
+   * This checks whether the guess already exists in the history. If it does,it
+   * does not add it. Otherwise, it adds the guess to the history and updates
+   * last_feedback_.
    *
    * @param guess The guess to add.
    */
   void AddToGuessHistory(const std::vector<int> &guess);
 
 private:
-  int difficulty_ = kEasyDifficulty;
+  const int kLifeStart = 10;
+
+  const int kEasyMinDigit = 0;
+  const int kEasyMaxDigit = 7;
+  const int kEasyCodeLength = 4;
+
+  const int kMediumMinDigit = 0;
+  const int kMediumMaxDigit = 8;
+  const int kMediumCodeLength = 4;
+
+  const int kHardMinDigit = 0;
+  const int kHardMaxDigit = 9;
+  const int kHardCodeLength = 4;
+
+  int difficulty_ = 1; // Default difficulty is easy
   int secret_code_min_digit_;
   int secret_code_max_digit_;
   int secret_code_length_;
 
   std::string player_name_;
   std::string game_name_;
+  int game_id_;
   int life_;
   int score_;
   std::vector<int> secret_code_;
