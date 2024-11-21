ea9947180a96e9065745db1ead5835ccbd2bde92
Quiyet Brul <qbrul0514@gmail.com>
2024-11-21 00:03:34 -0700

    logger test

---
+++ src/logger/logger_test.cpp
@@ -0,0 +1,52 @@
+#include "logger/logger.h"
+
+#include <fstream>
+#include <gtest/gtest.h>
+#include <string>
+
+class LoggerTest : public ::testing::Test {
+protected:
+  void SetUp() override {
+    // Prepare the log file
+    test_log_file_ = LOGGER_TEST_FILE_PATH;
+    Logger::GetInstance().SetOutputFile(test_log_file_);
+  }
+
+  void TearDown() override {
+    // Clean up the test log file
+    Logger::GetInstance().CloseOutputFile();
+    std::filesystem::remove(test_log_file_);
+  }
+
+  std::string test_log_file_;
+};
+
+TEST_F(LoggerTest, LogMessage) {
+  Logger &logger = Logger::GetInstance();
+  logger.Log("Test message");
+
+  // Verify the log file contains the message
+  std::ifstream log_file(test_log_file_);
+  ASSERT_TRUE(log_file.is_open());
+
+  std::string line;
+  bool found = false;
+  while (std::getline(log_file, line)) {
+    if (line.find("Test message") != std::string::npos) {
+      found = true;
+      break;
+    }
+  }
+
+  EXPECT_TRUE(found);
+}
+
+TEST_F(LoggerTest, FileInitialization) {
+  // Check if the log file is created
+  EXPECT_TRUE(std::filesystem::exists(test_log_file_));
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
