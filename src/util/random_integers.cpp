0ca0d8c7b65c2853fe206581db926383001f935c
Quiyet Brul <qbrul0514@gmail.com>
2024-11-20 21:38:28 -0700

    Update random_integers.cpp

--- src/util/random_integers.cpp
+++ src/util/random_integers.cpp
@@ -20,6 +20,18 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb,
   return size * nmemb;
 }
 
+// Fallback to pseudo-random number generator
+std::vector<int> FallbackRandomNumbers(const int &generate, const int &min,
+                                       const int &max) {
+  Logger &logger = Logger::GetInstance();
+  logger.Log("Falling back to pseudo-random number generator");
+  std::vector<int> random_number;
+  for (int i = 0; i < generate; i++) {
+    random_number.push_back(RandomNumber(min, max));
+  }
+  return random_number;
+}
+
 std::vector<int> GenRandom(const int &generate, const int &min,
                            const int &max) {
   CURL *curl;
@@ -30,44 +42,43 @@ std::vector<int> GenRandom(const int &generate, const int &min,
 
   logger.Log("Generating random numbers from random.org");
   curl = curl_easy_init();
-  if (curl) {
-    std::string url =
-        "https://www.random.org/integers/?num=" + std::to_string(generate) +
-        "&min=" + std::to_string(min) + "&max=" + std::to_string(max) +
-        "&col=1&base=10&format=plain&rnd=new";
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
-
-    res = curl_easy_perform(curl);
-
-    if (res == CURLE_OK) {
-      long http_code = 0;
-      curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
-      if (http_code == 503) {
-        logger.Log("Server is busy (503 Service Unavailable)");
-      } else {
-        std::istringstream iss(read_buffer);
-        std::string line;
-        while (std::getline(iss, line)) {
-          if (!line.empty()) {
-            random_number.push_back(std::stoi(line));
-          }
-        }
-        return random_number;
-      }
-    } else {
-      logger.Log("curl_easy_perform() failed" +
-                 std::string(curl_easy_strerror(res)));
-    }
-  } else {
+  if (!curl) {
     logger.Log("curl_easy_init() failed");
+    return FallbackRandomNumbers(generate, min, max);
   }
 
-  logger.Log("Falling back to pseudo-random number generator");
-  for (int i = 0; i < generate; i++) {
-    random_number.push_back(RandomNumber(min, max));
+  std::string url =
+      "https://www.random.org/integers/?num=" + std::to_string(generate) +
+      "&min=" + std::to_string(min) + "&max=" + std::to_string(max) +
+      "&col=1&base=10&format=plain&rnd=new";
+  curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
+
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK) {
+    logger.Log("curl_easy_perform() failed " +
+               std::string(curl_easy_strerror(res)));
+    curl_easy_cleanup(curl);
+    return FallbackRandomNumbers(generate, min, max);
   }
+
+  long http_code = 0;
+  curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+  curl_easy_cleanup(curl);
+  if (http_code == 503) {
+    logger.Log("Server is busy (503 Service Unavailable)");
+    return FallbackRandomNumbers(generate, min, max);
+  }
+
+  std::istringstream iss(read_buffer);
+  std::string line;
+  while (std::getline(iss, line)) {
+    if (!line.empty()) {
+      random_number.push_back(std::stoi(line));
+    }
+  }
+
   return random_number;
 }
 
